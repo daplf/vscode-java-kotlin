6c7856df104a67d31ebcbfa3ed0776f29febd88e
daplf <daplf96@gmail.com>
2022-03-23 15:30:23 +0000

    Setup delegateCommandHandler to set KLS classpath entry

--- src/extension.ts
+++ src/extension.ts
@@ -4,4 +4,15 @@ import * as vscode from 'vscode';
 
 /** Called when extension is activated */
 export async function activate(context: vscode.ExtensionContext) {
+    // We call vscode-kotlin's API to fetch the build output path.
+    // Note that this code is only executed after vscode-kotlin is setup, so the API should be ready to receive requests.
+    const vscodekotlinApi = vscode.extensions.getExtension('fwcd.kotlin').exports as ExtensionAPI;
+
+    // Execute a java workspace command to update the kotlin build output path. This triggers a classpath change on the JDT language server.
+    await vscode.commands.executeCommand("java.execute.workspaceCommand", "kotlin.java.setKotlinBuildOutput", vscodekotlinApi.getBuildOutputPath());
+}
+
+// vscode-kotlin API stub.
+interface ExtensionAPI {
+    getBuildOutputPath(): string;
 }
